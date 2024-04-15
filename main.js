40f1ed7653deee5be94a2efc6c055871f3589458
Naci Yalim <naciyalim1@gmail.com>
2024-04-15 17:00:36 +0200

    add data

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const app = Vue.createApp({
     return {
       product: "Socks",
       description: "the highest quality men's socks in the world ",
+      image: "./assets/images/socks_green.jpg",
+      url: "https://www.redbubble.com/i/socks/VueJS-In-Random-by-webdev27/118814720.9HZ1B",
     };
   },
 });
