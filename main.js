b8d486f8562253ffe13b4a499275ab03cf269c72
Naci Yalim <naciyalim1@gmail.com>
2024-04-15 18:08:30 +0200

    attrubute binding and conditional rendering

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const app = Vue.createApp({
       product: "Socks",
       description: "the highest quality men's socks in the world ",
       image: "./assets/images/socks_green.jpg",
+      onSale: true,
+      inStock: true,
+      inventory: 3,
       url: "https://www.redbubble.com/i/socks/VueJS-In-Random-by-webdev27/118814720.9HZ1B",
     };
   },
